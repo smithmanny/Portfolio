b4f40e32d9436ea3d69ca45e2a43d8a088be6826
Shakhor Smith <shakhorsmith@gmail.com>
2017-04-30 21:25:57 -0400

    Added smooth scrolling on click

--- js/main.js
+++ js/main.js
@@ -4,9 +4,42 @@ $(document).ready(function() {
   $('.overlay-btn').css('height', imgHeight);
   // Move active on menu click
   var links = $('.links a')
-  $('.links a').click(function() {
-    $('.links a').removeClass('active');
-    $(this).addClass('active');
+  $('a[href^="#"]').click(function(event) {
+    $('a[href^="#"]').removeClass('active');
+
+  var target = $(this.getAttribute('href'));
+
+if (target.length) {
+  event.preventDefault();
+  $('html, body').stop().animate({
+    scrollTop: target.offset().top
+  }, 500);
+  // Add class to show border
+  $(this).addClass('active');
+}
   });
+
+$(document).scroll(function() {
+
+  var scroll = $('.sticky-top').offset().top,
+      $about = $('#about').offset().top,
+      $port = $('#portfolio').offset().top,
+      $contact = $('#contact').offset().top;
+  // Check if Sticky Nav reached sections ID
+  if ($(this).scrollTop() < $port) {
+    links.removeClass('active');
+    $('.about a').addClass('active');
+  }
+  else if ($(this).scrollTop() < $contact && $(this).scrollTop() < 1317) {
+    links.removeClass('active');
+    $('.port a').addClass('active');
+  }
+  else {
+      links.removeClass('active');
+      $('.contact a').addClass('active');
+  }
+
+});
+
 
 });
