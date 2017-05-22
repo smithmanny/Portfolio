2c4fee661343b482998924446b6a61ef04fcdc1e
Shakhor Smith <shakhorsmith@gmail.com>
2017-05-22 19:56:37 -0400

    Switched from npm to yarn and added browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,65 @@
 // Include gulp
 var gulp = require('gulp');
- // Define base folders
+// Define base folders
 var src = 'src/';
 var dest = 'build/';
- // Include plugins
+// Include plugins
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-var sass = require('gulp-ruby-sass');
-var imagemin = require('gulp-imagemin');
-var cache = require('gulp-cache');
+var sass = require('gulp-sass');
+var bs = require('browser-sync');
+var reload = bs.reload;
 
- // Concatenate & Minify JS
+//Browser-Sync It's magic!!!
+gulp.task('browser-sync', function() {
+  bs.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+// Concatenate & Minify JS
 gulp.task('js', function() {
     return gulp.src(src + 'js/*.js')
-      .pipe(concat('main.js'))
-        .pipe(rename({suffix: '.min'}))
+        .pipe(concat('main.js'))
+        .pipe(rename({
+          suffix: '.min'
+        }))
         .pipe(uglify())
-        .pipe(gulp.dest(dest + 'js'));
+        .pipe(gulp.dest(dest + 'js'))
+        .pipe(bs.stream());
 });
- // Compile CSS from Sass files
+
+// Compile SASS into CSS
 gulp.task('sass', function() {
-    return sass('src/scss/style.scss', {style: 'compressed'})
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('build/css'));
+    return gulp.src(src + 'scss/*.scss')
+        .pipe(sass({
+          outputStyle: 'compressed'
+        }))
+        .pipe(rename({
+          suffix: '.min'
+        }))
+        .pipe(gulp.dest(dest + 'css'))
+        .pipe(bs.stream());
 });
- gulp.task('images', function() {
-  return gulp.src(src + 'images/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
-    .pipe(gulp.dest(dest + 'img'));
+
+gulp.task('watch', ['browser-sync'], function() {
+  // Watch .js files
+  gulp.watch(src + 'js/*.js', ['js']).on('change', reload);
+  // Watch .scss files
+  gulp.watch(src + 'scss/*.scss', ['sass']).on('change', reload);
+  // Watch HTML files
+  gulp.watch('*.html').on('change', reload);
 });
- // Watch for changes in files
-gulp.task('watch', function() {
-   // Watch .js files
-  gulp.watch(src + 'js/*.js', ['scripts']);
-   // Watch .scss files
-  gulp.watch(src + 'scss/*.scss', ['sass']);
-   // Watch image files
-  gulp.watch(src + 'images/**/*', ['images']);
- });
- // Default Task
-gulp.task('default', ['scripts', 'sass', 'images', 'watch']);
+
+// Default task
+gulp.task('default', ['js', 'sass', 'browser-sync'], function() {
+  // Watch .js files
+  gulp.watch(src + 'js/*.js', ['js']).on('change', reload);
+  // Watch .scss files
+  gulp.watch(src + 'scss/*.scss', ['sass']).on('change', reload);
+  // Watch HTML files
+  gulp.watch('*.html').on('change', reload);
+})
