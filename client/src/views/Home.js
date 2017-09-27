272fc6e910875609515a735a3ef5641d4469f1e0
Shakhor Smith <shakhorsmith@gmail.com>
2017-09-26 20:43:52 -0400

    Full Stack Developer?

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
-import { Container, Row, Col, Form, FormGroup, Input, Label } from "reactstrap";
-import axios from "axios";
-import "../styles/home.css";
-import "../font-mfizz/font-mfizz.css";
-import Resume from "../Shakhor_Smith_Resume.pdf";
-import Project_1 from "../img/project_1.jpeg";
-import Project_2 from "../img/project_2.jpeg";
-import Project_3 from "../img/project_3.jpeg";
+import React, { Component } from 'react';
+import { Container, Row, Col, Form, FormGroup, Input, Label } from 'reactstrap';
+import axios from 'axios';
+import '../styles/home.css';
+import '../font-mfizz/font-mfizz.css';
+import Resume from '../Shakhor_Smith_Resume.pdf';
+import Project_1 from '../img/project_1.jpeg';
+import Project_2 from '../img/project_2.jpeg';
+import Project_3 from '../img/project_3.jpeg';
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "",
-      email: "",
-      message: ""
+      name: '',
+      email: '',
+      message: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -30,17 +30,17 @@ class Home extends Component {
 
     const { name, email, message } = this.state;
 
-    axios.post('/api/form', { name, email, message })
+    axios.post('/api/form', { name, email, message });
     await this.clearForm();
-  };
+  }
 
   // Clear Form after submit
   clearForm() {
     this.setState({
-      name: "",
-      email: "",
-      message: "",
-      error: ""
+      name: '',
+      email: '',
+      message: '',
+      error: ''
     });
   }
 
@@ -49,7 +49,7 @@ class Home extends Component {
       <div>
         <div id="intro">
           <h1>Shakhor Smith</h1>
-          <h1>WordPress & Front-End Developer</h1>
+          <h1>Full Stack Developer</h1>
         </div>
 
         {/* About Me Section */}
@@ -57,16 +57,7 @@ class Home extends Component {
           <Container>
             <h1 className="text-center">About Me</h1>
             <p className="text-center">
-              My name is Shakhor Smith and I'm a WordPress & Front-End Web
-              Developer based in Atlanta, GA. I found my passion for Web
-              Development in high school and decided to teach myself with the
-              help of{" "}
-              <a href="http://referrals.trhou.se/shakhorsmith">
-                Team Treehouse
-              </a>. I continue to learn everyday to purse my goal of becoming a
-              Full Stack Developer and eventually transition into mobile
-              applications as well. Outside of developing I'm adventurous so you
-              can catch me traveling the world and spending time with family.{" "}
+Hi, my name is Shakhor Smith and I'm a Full Stack Developer with experience in both LAMP and MERN stack. During work, I'm working with PHP, MySQL, and Vue. On my off hours I transform into a Javascript ninja working with MongoDB, Express, React JS, and Node. I'm in the process of switching into a software engineer working with Full Stack Javascript as well as Java to build applications for both web and mobile.
             </p>
             <div className="skills">
               <div className="mb-2">
@@ -74,7 +65,7 @@ class Home extends Component {
                 <i className="icon-gulp" alt="Gulp" />
                 <i className="icon-git" alt="Git" />
                 <i className="icon-mysql" alt="MySQL" />
-                {/* <i className="icon-mongodb" /> */}
+                <i className="icon-mongodb" />
               </div>
               {/* Front-End Languages & Frameworks */}
               <div>
@@ -84,7 +75,7 @@ class Home extends Component {
                 <i className="icon-bootstrap" alt="Bootstrap" />
                 {/* <i className="icon-javascript" /> */}
                 <i className="icon-jquery" alt="Jquery" />
-                {/* <i className="icon-reactjs" /> */}
+                <i className="icon-reactjs" /> 
                 <i className="icon-php" alt="PHP" />
                 <i className="icon-symfony" alt="Symfony" />
               </div>
@@ -112,7 +103,8 @@ class Home extends Component {
                   <div className="projectInfo my-4">
                     <h2 className="text-center">Sharonne Lanier</h2>
                     <p className="m-0 text-center">
-                      WordPress website designed and developed for actress Sharonne Lanier
+                      WordPress website designed and developed for actress
+                      Sharonne Lanier
                     </p>
                   </div>
                   <div className="projectSkills my-4">
@@ -130,8 +122,8 @@ class Home extends Component {
                         <i className="icon-bootstrap" alt="Bootstrap" />
                       </Col>
                       <Col>
-                          <i className="icon-jquery" alt="jQuery" />
-                        </Col>
+                        <i className="icon-jquery" alt="jQuery" />
+                      </Col>
                       <Col>
                         <i className="icon-php" alt="PHP" />
                       </Col>
@@ -141,11 +133,12 @@ class Home extends Component {
                     </Row>
                   </div>
                   <div className="text-center my-4 code">
-                    <a href="https://github.com/smithmanny/Sharonne-Lanier"
-                    target="_blank" rel="noopener noreferrer">
-                      <button>
-                      View Project
-                      </button>
+                    <a
+                      href="https://github.com/smithmanny/Sharonne-Lanier"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <button>View Project</button>
                     </a>
                   </div>
                 </div>
@@ -164,7 +157,8 @@ class Home extends Component {
                   <div className="projectInfo my-4">
                     <h2 className="text-center">Emaily App</h2>
                     <p className="m-0 text-center">
-                      App to send out email surveys built with Node + React + Redux Form + MongoDB
+                      App to send out email surveys built with Node + React +
+                      Redux Form + MongoDB
                     </p>
                   </div>
                   <div className="projectSkills my-4">
@@ -176,8 +170,8 @@ class Home extends Component {
                         <i className="icon-css3" alt="CSS3" />
                       </Col>
                       <Col>
-                          <i className="icon-reactjs" alt="React" />
-                        </Col>
+                        <i className="icon-reactjs" alt="React" />
+                      </Col>
                       <Col>
                         <i className="icon-nodejs" alt="Node" />
                       </Col>
@@ -187,12 +181,19 @@ class Home extends Component {
                     </Row>
                   </div>
                   <div className="text-center my-4">
-                    <a href="https://github.com/smithmanny/Emaily"
-                    target="_blank" rel="noopener noreferrer">
+                    <a
+                      href="https://github.com/smithmanny/Emaily"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       <button>View Code</button>
                     </a>
-                    <a href="https://aqueous-stream-25587.herokuapp.com/"
-                    className="mx-1" target="_blank" rel="noopener noreferrer">
+                    <a
+                      href="https://aqueous-stream-25587.herokuapp.com/"
+                      className="mx-1"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       <button>View Project</button>
                     </a>
                   </div>
@@ -224,20 +225,28 @@ class Home extends Component {
                         <i className="icon-css3" alt="CSS3" />
                       </Col>
                       <Col>
-                          <i className="icon-reactjs" alt="React" />
-                        </Col>
+                        <i className="icon-reactjs" alt="React" />
+                      </Col>
                       <Col>
                         <i className="icon-heroku" alt="Heroku" />
                       </Col>
                     </Row>
                   </div>
                   <div className="text-center my-4">
-                    <a href="https://github.com/smithmanny/Todo-App"
-                    target="_blank" rel="noopener noreferrer" cursor="pointer">
+                    <a
+                      href="https://github.com/smithmanny/Todo-App"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      cursor="pointer"
+                    >
                       <button>View Code</button>
                     </a>
-                    <a href="https://smithmanny.github.io/Todo-App/"
-                    className="mx-1" target="_blank" rel="noopener noreferrer">
+                    <a
+                      href="https://smithmanny.github.io/Todo-App/"
+                      className="mx-1"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       <button>View Project</button>
                     </a>
                   </div>
@@ -319,10 +328,9 @@ class Home extends Component {
                     aria-hidden="true"
                   />
                 </a>
-                <a href={Resume} rel="noopener" target="_blank" alt="Resume"><i
-                  className="fa fa-file-pdf-o"
-                  aria-hidden="true"/>
-              </a>
+                <a href={Resume} rel="noopener" target="_blank" alt="Resume">
+                  <i className="fa fa-file-pdf-o" aria-hidden="true" />
+                </a>
               </div>
             </div>
           </Container>
@@ -330,14 +338,16 @@ class Home extends Component {
 
         <footer id="footer">
           <div id="copyright">
-            <small>Designed and developed by{" "}
-            <a
-              href="https://shakhorsmith.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Shakhor Smith
-            </a></small>
+            <small>
+              Designed and developed by{' '}
+              <a
+                href="https://shakhorsmith.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Shakhor Smith
+              </a>
+            </small>
           </div>
         </footer>
       </div>
