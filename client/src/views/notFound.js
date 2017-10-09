8b665a7f3e4288368f6047e808dedf9278f1d14e
Shakhor Smith <shakhorsmith@gmail.com>
2017-10-08 23:37:22 -0400

    Add some styling to 404 page

--- client/src/views/notFound.js
+++ client/src/views/notFound.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import '../styles/notFound.css';
 
 const NotFound = () => {
   return (
-    <div>
+    <div className="container">
       <Helmet>
         <title>Page Not Found | Shakhor Smith</title>
       </Helmet>
-      <h1>Page not found</h1>
+      <div className='my-3'>
+        <h1>Danger Zone!</h1>
+        <h4>The page you are looking for can not be found</h4>
+      </div>
+      <div id='notFound'></div>
     </div>
   )
 }
