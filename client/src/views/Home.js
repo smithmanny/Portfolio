e9159134d78c665604890037f9a234115b20da01
Shakhor Smith <shakhorsmith@gmail.com>
2017-12-28 22:48:32 -0500

    Changed to light theme

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -5,9 +5,6 @@ import axios from 'axios';
 import '../styles/home.css';
 import '../font-mfizz/font-mfizz.css';
 import Resume from '../Shakhor_Smith_Resume.pdf';
-import Project_1 from '../img/project_1.jpeg';
-import Project_2 from '../img/project_2.jpeg';
-import Project_3 from '../img/project_3.jpeg';
 // Form Views
 import DefaultView from './form/DefaultView';
 import ErrorView from './form/ErrorView';
@@ -88,13 +85,7 @@ class Home extends Component {
           <Container>
             <h2 className="text-center">About Me</h2>
             <p className="text-center">
-              Hi, my name is Shakhor Smith and I'm a Full Stack Developer with
-              experience in both LAMP and MERN stack. During work, I'm working
-              with PHP, MySQL, and Vue. On my off hours I transform into a
-              Javascript ninja working with MongoDB, Express, React JS, and
-              Node. I'm in the process of switching into a software engineer
-              working with Full Stack Javascript as well as Java to build
-              applications for both web and mobile.
+              Hi, my name is Shakhor Smith and I'm a Full Stack JavaScript Developer working with React.js and Node.js to build web apps. Most of my projects are built using the MERN stack, but every so often you can catch me developing in the LAMP stack as well.
             </p>
             <div className="skills">
               <div className="mb-2">
@@ -129,165 +120,72 @@ class Home extends Component {
             <Row>
               {/* Project 1 */}
               <Col xs="12" md="6" lg="4" className="mb-4">
-                <div className="projectWrapper level-3">
-                  <div className="projectImg">
-                    <img
-                      src={Project_1}
-                      alt="Github Logo"
-                      className="img-fluid"
-                    />
-                  </div>
-                  <div className="projectInfo my-4">
-                    <h2 className="text-center">Sharonne Lanier</h2>
-                    <p className="m-0 text-center">
-                      WordPress website designed and developed for actress
-                      Sharonne Lanier
-                    </p>
-                  </div>
-                  <div className="projectSkills my-4">
-                    <Row>
-                      <Col>
-                        <i className="icon-html5" alt="HTML5" />
-                      </Col>
-                      <Col>
-                        <i className="icon-css3" alt="CSS3" />
-                      </Col>
-                      <Col>
-                        <i className="icon-sass" alt="Sass" />
-                      </Col>
-                      <Col>
-                        <i className="icon-bootstrap" alt="Bootstrap" />
-                      </Col>
-                      <Col>
-                        <i className="icon-jquery" alt="jQuery" />
-                      </Col>
-                      <Col>
-                        <i className="icon-php" alt="PHP" />
-                      </Col>
-                      <Col>
-                        <i className="icon-wordpress" alt="WordPress" />
-                      </Col>
-                    </Row>
-                  </div>
-                  <div className="text-center my-4 code">
-                    <a
-                      href="https://github.com/smithmanny/Sharonne-Lanier"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <button>View Project</button>
-                    </a>
-                  </div>
+                <div className="projectWrapper level-3 text-center">
+                  <h2>Sharonne Lanier</h2>
+                  <p>
+                    WordPress website designed and developed for actress
+                    Sharonne Lanier
+                  </p>
+                  <a
+                    href="https://github.com/smithmanny/Sharonne-Lanier"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <button>View Project</button>
+                  </a>
                 </div>
               </Col>
 
               {/* Project 2 */}
               <Col xs="12" md="6" lg="4" className="mb-4">
-                <div className="projectWrapper level-3">
-                  <div className="projectImg">
-                    <img
-                      src={Project_2}
-                      alt="Github Logo"
-                      className="img-fluid"
-                    />
-                  </div>
-                  <div className="projectInfo my-4">
-                    <h2 className="text-center">Emaily App</h2>
-                    <p className="m-0 text-center">
-                      App to send out email surveys built with Node + React +
-                      Redux Form + MongoDB
-                    </p>
-                  </div>
-                  <div className="projectSkills my-4">
-                    <Row>
-                      <Col>
-                        <i className="icon-html5" alt="HTML5" />
-                      </Col>
-                      <Col>
-                        <i className="icon-css3" alt="CSS3" />
-                      </Col>
-                      <Col>
-                        <i className="icon-reactjs" alt="React" />
-                      </Col>
-                      <Col>
-                        <i className="icon-nodejs" alt="Node" />
-                      </Col>
-                      <Col>
-                        <i className="icon-mongodb" alt="Jquery" />
-                      </Col>
-                    </Row>
-                  </div>
-                  <div className="text-center my-4">
-                    <a
-                      href="https://github.com/smithmanny/Emaily"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <button>View Code</button>
-                    </a>
-                    <a
-                      href="https://aqueous-stream-25587.herokuapp.com/"
-                      className="mx-1"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <button>View Project</button>
-                    </a>
-                  </div>
+                <div className="projectWrapper level-3 text-center">
+                  <h2>Emaily App</h2>
+                  <p>
+                    App to send out email surveys built with Node + React +
+                    Redux Form + MongoDB
+                  </p>
+                  <a
+                    href="https://github.com/smithmanny/Emaily"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <button>View Code</button>
+                  </a>
+                  <a
+                    href="https://aqueous-stream-25587.herokuapp.com/"
+                    className="mx-1"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <button>View Project</button>
+                  </a>
                 </div>
               </Col>
 
               {/* Project 3 */}
               <Col xs="12" md="6" lg="4" className="mb-4">
-                <div className="projectWrapper level-3">
-                  <div className="projectImg">
-                    <img
-                      src={Project_3}
-                      alt="Github Logo"
-                      className="img-fluid"
-                    />
-                  </div>
-                  <div className="projectInfo my-4">
-                    <h2 className="text-center">Home Decor</h2>
-                    <p className="m-0 text-center">
-                      Home Decor website where clients can request quotes to
-                      rent furniture
-                    </p>
-                  </div>
-                  <div className="projectSkills my-4">
-                    <Row>
-                      <Col>
-                        <i className="icon-html5" alt="HTML5" />
-                      </Col>
-                      <Col>
-                        <i className="icon-css3" alt="CSS3" />
-                      </Col>
-                      <Col>
-                        <i className="icon-php" alt="PHP" />
-                      </Col>
-                      <Col>
-                        <i className="icon-heroku" alt="Heroku" />
-                      </Col>
-                    </Row>
-                  </div>
-                  <div className="text-center my-4">
-                    <a
-                      href="https://github.com/smithmanny/Khadija-Project"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      cursor="pointer"
-                    >
-                      <button>View Code</button>
-                    </a>
-                    <a
-                      href="https://khadija.herokuapp.com/"
-                      className="mx-1"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <button>View Project</button>
-                    </a>
-                  </div>
+                <div className="projectWrapper level-3 text-center">
+                  <h2>Home Decor</h2>
+                  <p>
+                    Home Decor website where clients can request quotes to
+                    rent furniture
+                  </p>
+                  <a
+                    href="https://github.com/smithmanny/Khadija-Project"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    cursor="pointer"
+                  >
+                    <button>View Code</button>
+                  </a>
+                  <a
+                    href="https://khadija.herokuapp.com/"
+                    className="mx-1"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <button>View Project</button>
+                  </a>
                 </div>
               </Col>
             </Row>
--- client/src/views/notFound.js
+++ client/src/views/notFound.js
@@ -1,20 +1,26 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom'
+
+import logo  from '../img/1.png'
 import '../styles/notFound.css';
 
 const NotFound = () => {
   return (
-    <div className="container">
+    <div className="main">
       <Helmet>
         <title>Page Not Found | Shakhor Smith</title>
       </Helmet>
-      <div className='my-3'>
-        <h1>Danger Zone!</h1>
-        <h4>The page you are looking for can not be found</h4>
+      <div className="back_home">
+        <Link to="/"><h5>Back Home</h5></Link>
       </div>
-      <div id='notFound'></div>
-    </div>
-  )
-}
+      <img
+        src={logo}
+        alt="Background"
+        className="notFoundBanner" />
 
-export default NotFound
+    </div>
+  );
+};
+
+export default NotFound;
