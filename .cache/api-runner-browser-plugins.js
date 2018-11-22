fed015539ac44db6bffe2bfa124f742193b85ce1
Shakhor Smith <shakhorsmith@gmail.com>
2018-11-21 21:05:50 -0500

    Add amplitude seo

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,4 +1,7 @@
 module.exports = [{
+      plugin: require('/Users/shakhor/Projects/Web/Portfolio/node_modules/gatsby-plugin-amplitude-analytics/gatsby-browser'),
+      options: {"plugins":[],"head":false,"respectDNT":true,"exclude":["/preview/**","/do-not-track/me/too/"],"eventTypes":{"outboundLinkClick":"OUTBOUND_LINK_CLICK","pageView":"PAGE_VIEW"},"amplitudeConfig":{"saveEvents":true,"includeUtm":true,"includeReferrer":true}},
+    },{
       plugin: require('/Users/shakhor/Projects/Web/Portfolio/node_modules/gatsby-plugin-offline/gatsby-browser'),
       options: {"plugins":[]},
     },{
